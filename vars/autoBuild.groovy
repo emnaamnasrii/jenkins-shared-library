a72dcfd56faa8fbdfa4531db2575830c69e86d15
emnaamnasrii <emna.mnasri@esprit.tn>
2026-03-07 22:51:27 +0100

    Fix: Network access and language detection for large repos

--- vars/autoBuild.groovy
+++ vars/autoBuild.groovy
@@ -195,18 +195,19 @@ CMD ["npm","start"]
 else if (language == "java-maven" || language == "java-gradle") {
     dockerfileContent = """
 # Étape 1 : builder le JAR
-FROM maven:3.9.1-eclipse-temurin-17 AS build
+FROM maven:3.9.9-eclipse-temurin-17 AS build
 WORKDIR /app
 COPY pom.xml .
 COPY src ./src
-RUN mvn clean package -DskipTests -Dcheckstyle.skip=true
+RUN mvn clean package -DskipTests
 
 # Étape 2 : exécuter le JAR
-FROM eclipse-temurin:17-jdk-slim
+FROM eclipse-temurin:17-jdk
 WORKDIR /app
 COPY --from=build /app/target/*.jar app.jar
+
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["java","-jar","app.jar"]
 """
 }
 
